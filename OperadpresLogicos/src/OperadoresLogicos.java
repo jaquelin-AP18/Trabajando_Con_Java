f6d93ad4cbcfb6381ffb43a987ab0fc716d5fc40
jaquelin <a.jaquelinperez@gmail.com>
2021-07-15 18:27:02 -0500

    Operadores lógicos Java

---
+++ OperadpresLogicos/src/OperadoresLogicos.java
@@ -0,0 +1,39 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Operadores aritmeticos
+		 * a + b
+		 * a - b
+		 * a * b
+		 * a / b
+		 * a % b
+		 * 
+		 * Operadores de comparación
+		 * a == b "a es igual a b"
+		 * a > b a es mayor que b
+		 * a< b a es menor que b
+		 * a!=b a es diferente que b
+		 * a >= b "a es mayor o igual a b"
+		 * a <= b "a es menor o igual a b"
+		 * 
+		 * Operadores lógicos
+		 * ! 
+		 * |
+		 * &
+		 * ||
+		 * &&
+		 */
+		
+		
+		System.out.println(saludo());
+	}
+	
+	//Creamos un metodo publico, estatico(no se requiere instanciar ningun objeto) y de tipo string
+	public static String saludo() {
+		return"Saludo";
+	}
+}
