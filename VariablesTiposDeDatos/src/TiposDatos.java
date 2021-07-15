7f80e6039a23a3d653a12cfe0c75947a3c17c95b
jaquelin <a.jaquelinperez@gmail.com>
2021-07-15 18:33:03 -0500

    Variables y tipos de datos en java

---
+++ VariablesTiposDeDatos/src/TiposDatos.java
@@ -0,0 +1,21 @@
+
+public class TiposDatos {
+	/**Se refiere a que es un metodo estatico **/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Mundo");
+		
+		int numero1=30;
+		int numero2=6;
+		int multiplicacion=numero1*numero2;
+		String cadena1="texto 1";
+		String cadena2="texto 2";
+		String oracion=cadena1+cadena2;
+		char letra='y';
+		boolean mayor18=true;
+		
+		System.out.println(oracion+letra);
+	}
+
+}
